a7f89a9d9ed4f1e8232a58e5f1e3d4c9e9ca4cca
PeterGodoy <67289778+PeterGodoy@users.noreply.github.com>
2022-12-29 21:09:17 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Write a function that takes three(3) parameters (name, height and country)
+//Write an alert in the function that prints out the inputs.
+function myFunction(Name, height, country) {
+    alert("Hello " + Name +" you are " + height + " inches and live in the " + country) 
+}
+//Call the function and pass the three(3) parameter values into it.
+myFunction("Peter", 70, "USA");
+
+
+//Push your codes to GitHub
